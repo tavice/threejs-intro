9f0bfa60ad7f4bb5ef0d5c3c45b061a15c4cbf4c
tavice <thomasavice.ta@gmail.com>
2023-07-07 15:25:33 -0700

    upload pdf and svg

---
+++ src/components/FileUploader.js
@@ -0,0 +1,85 @@
+import React, { useState, useEffect } from 'react';
+import { Document, Page, pdfjs } from 'react-pdf';
+import pdf2svg from 'pdf2svg';
+
+pdfjs.GlobalWorkerOptions.workerSrc = new URL(
+  'pdfjs-dist/build/pdf.worker.min.js',
+  import.meta.url
+).toString();
+
+function FileUploader() {
+  const [uploadedFile, setUploadedFile] = useState(null);
+  const [numPages, setNumPages] = useState(null);
+  const [svgData, setSvgData] = useState(null);
+
+  const handleFileUpload = async (event) => {
+    const file = event.target.files[0];
+    setUploadedFile(URL.createObjectURL(file));
+    await convertToSVG(file);
+  };
+
+  const handleDrop = async (event) => {
+    event.preventDefault();
+    const file = event.dataTransfer.files[0];
+    setUploadedFile(URL.createObjectURL(file));
+    await convertToSVG(file);
+  };
+
+  const onDocumentLoadSuccess = ({ numPages }) => {
+    setNumPages(numPages);
+  };
+
+  const convertToSVG = async (file) => {
+    const reader = new FileReader();
+    reader.onload = async (event) => {
+      const arrayBuffer = event.target.result;
+      const result = await pdf2svg(arrayBuffer, { width: 800, height: 600 });
+
+      setSvgData(result.svg);
+    };
+    reader.readAsArrayBuffer(file);
+  };
+
+  return (
+    <div
+      onDragOver={(event) => event.preventDefault()}
+      onDrop={handleDrop}
+      style={{
+        border: '2px dashed #000',
+        padding: '20px',
+        width: '100%',
+        height: '100%',
+        display: 'flex',
+      }}
+    >
+      <div style={{ flex: 1 }}>
+        <h3>Upload PDF File</h3>
+        <input type="file" onChange={handleFileUpload} />
+        {uploadedFile && (
+          <div style={{ marginTop: '20px', width: '100%', height: '100%' }}>
+            <Document
+              file={uploadedFile}
+              onLoadSuccess={onDocumentLoadSuccess}
+            >
+              {Array.from(new Array(numPages), (_, index) => (
+                <Page
+                  key={index + 1}
+                  pageNumber={index + 1}
+                  width={window.innerWidth * 0.8}
+                  height={window.innerHeight * 0.8}
+                />
+              ))}
+            </Document>
+          </div>
+        )}
+      </div>
+      <div style={{ flex: 1, marginLeft: '20px' }}>
+        {svgData && (
+          <div dangerouslySetInnerHTML={{ __html: svgData }} />
+        )}
+      </div>
+    </div>
+  );
+}
+
+export default FileUploader;
